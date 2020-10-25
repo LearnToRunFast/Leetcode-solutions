4e99edee6079e0459deeaebc3872e11695951542
Jiang <jiangqinhua1993@gmail.com>
2020-10-25 10:35:52 +0800

    dfs

---
+++ code/297.二叉树的序列化与反序列化.java
@@ -0,0 +1,91 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+import javax.swing.tree.TreeNode;
+
+/*
+ * @lc app=leetcode.cn id=297 lang=java
+ *
+ * [297] 二叉树的序列化与反序列化
+ *
+ * https://leetcode-cn.com/problems/serialize-and-deserialize-binary-tree/description/
+ *
+ * algorithms
+ * Hard (51.77%)
+ * Likes:    381
+ * Dislikes: 0
+ * Total Accepted:    53.5K
+ * Total Submissions: 102K
+ * Testcase Example:  '[1,2,3,null,null,4,5]'
+ *
+ * 
+ * 序列化是将一个数据结构或者对象转换为连续的比特位的操作，进而可以将转换后的数据存储在一个文件或者内存中，同时也可以通过网络传输到另一个计算机环境，采取相反方式重构得到原数据。
+ * 
+ * 请设计一个算法来实现二叉树的序列化与反序列化。这里不限定你的序列 /
+ * 反序列化算法执行逻辑，你只需要保证一个二叉树可以被序列化为一个字符串并且将这个字符串反序列化为原始的树结构。
+ * 
+ * 示例: 
+ * 
+ * 你可以将以下二叉树：
+ * 
+ * ⁠   1
+ * ⁠  / \
+ * ⁠ 2   3
+ * ⁠    / \
+ * ⁠   4   5
+ * 
+ * 序列化为 "[1,2,3,null,null,4,5]"
+ * 
+ * 提示: 这与 LeetCode 目前使用的方式一致，详情请参阅 LeetCode
+ * 序列化二叉树的格式。你并非必须采取这种方式，你也可以采用其他的方法解决这个问题。
+ * 
+ * 说明: 不要使用类的成员 / 全局 / 静态变量来存储状态，你的序列化和反序列化算法应该是无状态的。
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Codec {
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        if (root == null) return "null,";
+        StringBuilder sb = new StringBuilder();
+        sb.append(root.val);
+        sb.append(",");
+        sb.append(serialize(root.left));
+        sb.append(serialize(root.right));
+        return sb.toString();
+    }
+
+    private TreeNode deserialize(Queue<String> queue) {
+        String value = queue.poll();
+        if (value.equals("null")) return null;
+        
+        TreeNode root = new TreeNode(Integer.valueOf(value));
+        root.left = deserialize(queue);
+        root.right = deserialize(queue);
+        return root;
+    }
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        Queue<String> queue = new LinkedList<>(Arrays.asList(data.split(",")));
+        return deserialize(queue);
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec ser = new Codec();
+// Codec deser = new Codec();
+// TreeNode ans = deser.deserialize(ser.serialize(root));
+// @lc code=end
+
