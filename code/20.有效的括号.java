cb92e1925da5f2132949fee7a0b5be056a9adb7e
Jiang <jiangqinhua1993@gmail.com>
2020-11-02 23:04:22 +0800

    Valid Parentheses: Stack

---
+++ code/20.有效的括号.java
@@ -0,0 +1,84 @@
+import java.util.LinkedList;
+
+/*
+ * @lc app=leetcode.cn id=20 lang=java
+ *
+ * [20] 有效的括号
+ *
+ * https://leetcode-cn.com/problems/valid-parentheses/description/
+ *
+ * algorithms
+ * Easy (42.88%)
+ * Likes:    1949
+ * Dislikes: 0
+ * Total Accepted:    447.7K
+ * Total Submissions: 1M
+ * Testcase Example:  '"()"'
+ *
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+ * 
+ * 有效字符串需满足：
+ * 
+ * 
+ * 左括号必须用相同类型的右括号闭合。
+ * 左括号必须以正确的顺序闭合。
+ * 
+ * 
+ * 注意空字符串可被认为是有效字符串。
+ * 
+ * 示例 1:
+ * 
+ * 输入: "()"
+ * 输出: true
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: "()[]{}"
+ * 输出: true
+ * 
+ * 
+ * 示例 3:
+ * 
+ * 输入: "(]"
+ * 输出: false
+ * 
+ * 
+ * 示例 4:
+ * 
+ * 输入: "([)]"
+ * 输出: false
+ * 
+ * 
+ * 示例 5:
+ * 
+ * 输入: "{[]}"
+ * 输出: true
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isValid(String s) {
+        if ((s.length() & 1) == 1) return false;
+
+        LinkedList<Character> stack = new LinkedList<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) return false;
+
+                char open = stack.pop();
+                if (c == ')' && open != '(' ||
+                    c == ']' && open != '[' ||
+                    c == '}' && open != '{') {
+                        return false;
+                    }
+            }
+        }
+        return stack.isEmpty() ? true : false;
+    }
+}
+// @lc code=end
+
