6f986d0525f9324986dc0b5c20c78b527c22cb07
Jiang <jiangqinhua1993@gmail.com>
2020-11-05 12:52:57 +0800

    Word Break: DP, Trie

--- code/139.单词拆分.java
+++ code/139.单词拆分.java
@@ -51,24 +51,119 @@ import java.util.Set;
  */
 
 // @lc code=start
-public class Solution {
-    public boolean wordBreak(String s, List<String> wordDict) {
-        Set<String> set = new HashSet(wordDict);
-        int n = s.length();
+// public class Solution {
+//     public boolean wordBreak(String s, List<String> wordDict) {
+//         Set<String> set = new HashSet(wordDict);
+//         int n = s.length();
 
-        List<Integer> dp = new ArrayList<>();
-        dp.add(0);
-        for (int end = 1; end <= n; end++) {
-            for (int start : dp) {
-                if (set.contains(s.substring(start, end))) {
-                    dp.add(end);
-                    break;
+//         List<Integer> dp = new ArrayList<>();
+//         dp.add(0);
+//         for (int end = 1; end <= n; end++) {
+//             for (int start : dp) {
+//                 if (set.contains(s.substring(start, end))) {
+//                     dp.add(end);
+//                     break;
+//                 }
+//             }
+//         }
+//         return dp.get(dp.size() - 1) == n;
+//     }
+// }
+
+public class Solution {
+    class TrieNode {
+        private final int R = 26;
+        private boolean isEnd;
+        private TrieNode[] links;
+
+        public TrieNode() {
+            links = new TrieNode[R];
+        }
+
+        public boolean containsKey(char c) {
+            return links[c - 'a'] != null;
+        }
+
+        public TrieNode get(char c) {
+            return links[c - 'a'];
+        }
+        
+        public void put(char c) {
+            links[c - 'a'] = new TrieNode();
+        }
+
+        public void setEnd() {
+            this.isEnd = true;
+        }
+
+        public boolean match() {
+            return this.isEnd;
+        }
+
+    }
+    class Trie {
+        private TrieNode root;
+        private boolean searchResult;
+        private int[] isVisited; 
+        private String word;
+        private int n;
+
+        public Trie() {
+            root = new TrieNode();
+        }
+
+        public void insert(String word) {
+            TrieNode node = root;
+            for (char c : word.toCharArray()) {
+                if (!node.containsKey(c)) {
+                    node.put(c);
+                }
+                node = node.get(c);
+            }
+            node.setEnd();
+        }
+
+        private void backtrack(int idx, TrieNode curr) {
+            if (searchResult || idx == n || isVisited[idx] != 0) return;
+
+            isVisited[idx] = 1;
+            for (int i = idx; i < n && !searchResult; i++) {
+                char c = word.charAt(i);
+
+                if (!curr.containsKey(c)) {
+                    return;
+                }
+                curr = curr.get(c);
+                if (curr.match()) {
+                    if (i == n - 1) {
+                        searchResult = true;
+                        return;
+                    }
+
+                    if(i < n - 1 && isVisited[i + 1] == 0) {
+                        backtrack(i + 1, root);
+                    }
                 }
             }
         }
-        return dp.get(dp.size() - 1) == n;
+        public boolean search(String s) {
+            word = s;
+            n = word.length();
+            searchResult = false;
+            isVisited = new int[n];
+            backtrack(0, root);
+            return searchResult;
+        }
+    }
+
+   public boolean wordBreak(String s, List<String> wordDict) {
+        Trie trie = new Trie();
+        for (String word : wordDict) {
+            trie.insert(word);
+        }
+
+        return trie.search(s);
     }
 }
-
 // @lc code=end
 
