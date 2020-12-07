f200919fb5407dcdd9697134023f9555a804c8e1
Jiang <Jiangqinhua1993@gmail.com>
2020-12-07 23:15:55 +0800

    Binary Search Tree Iterator: Stack

---
+++ code/173.二叉搜索树迭代器.java
@@ -0,0 +1,103 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+import javax.swing.tree.TreeNode;
+
+/*
+ * @lc app=leetcode.cn id=173 lang=java
+ *
+ * [173] 二叉搜索树迭代器
+ *
+ * https://leetcode-cn.com/problems/binary-search-tree-iterator/description/
+ *
+ * algorithms
+ * Medium (75.09%)
+ * Likes:    291
+ * Dislikes: 0
+ * Total Accepted:    33.8K
+ * Total Submissions: 45K
+ * Testcase Example:  '["BSTIterator","next","next","hasNext","next","hasNext","next","hasNext","next","hasNext"]\n' +
+  '[[[7,3,15,null,null,9,20]],[],[],[],[],[],[],[],[],[]]'
+ *
+ * 实现一个二叉搜索树迭代器。你将使用二叉搜索树的根节点初始化迭代器。
+ * 
+ * 调用 next() 将返回二叉搜索树中的下一个最小的数。
+ * 
+ * 
+ * 
+ * 示例：
+ * 
+ * 
+ * 
+ * BSTIterator iterator = new BSTIterator(root);
+ * iterator.next();    // 返回 3
+ * iterator.next();    // 返回 7
+ * iterator.hasNext(); // 返回 true
+ * iterator.next();    // 返回 9
+ * iterator.hasNext(); // 返回 true
+ * iterator.next();    // 返回 15
+ * iterator.hasNext(); // 返回 true
+ * iterator.next();    // 返回 20
+ * iterator.hasNext(); // 返回 false
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * next() 和 hasNext() 操作的时间复杂度是 O(1)，并使用 O(h) 内存，其中 h 是树的高度。
+ * 你可以假设 next() 调用总是有效的，也就是说，当调用 next() 时，BST 中至少存在一个下一个最小的数。
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class BSTIterator {
+
+    Deque<TreeNode> stack;
+    public BSTIterator(TreeNode root) {
+        stack = new LinkedList<TreeNode>();
+        construct(root);
+    }
+    private void construct(TreeNode root) {
+        while (root != null) {
+            stack.push(root);
+            root = root.left;
+        }
+    }
+    public int next() {
+        TreeNode curr = stack.pop();
+        if (curr.right != null) {
+            construct(curr.right);
+        }
+        return curr.val;
+    }
+    
+    public boolean hasNext() {
+        return !stack.isEmpty();
+    }
+}
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator obj = new BSTIterator(root);
+ * int param_1 = obj.next();
+ * boolean param_2 = obj.hasNext();
+ */
+// @lc code=end
+
