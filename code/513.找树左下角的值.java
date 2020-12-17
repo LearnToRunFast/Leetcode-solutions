6148c3fedb5a2642ee049ed6e8dcd3b664de8482
Jiang <Jiangqinhua1993@gmail.com>
2020-12-17 21:16:30 +0800

    Find Bottom Left Tree Value:BFS

---
+++ code/513.找树左下角的值.java
@@ -0,0 +1,96 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+import javax.swing.tree.TreeNode;
+
+/*
+ * @lc app=leetcode.cn id=513 lang=java
+ *
+ * [513] 找树左下角的值
+ *
+ * https://leetcode-cn.com/problems/find-bottom-left-tree-value/description/
+ *
+ * algorithms
+ * Medium (71.88%)
+ * Likes:    140
+ * Dislikes: 0
+ * Total Accepted:    25.4K
+ * Total Submissions: 35.3K
+ * Testcase Example:  '[2,1,3]'
+ *
+ * 给定一个二叉树，在树的最后一行找到最左边的值。
+ * 
+ * 示例 1:
+ * 
+ * 
+ * 输入:
+ * 
+ * ⁠   2
+ * ⁠  / \
+ * ⁠ 1   3
+ * 
+ * 输出:
+ * 1
+ * 
+ * 
+ * 
+ * 
+ * 示例 2: 
+ * 
+ * 
+ * 输入:
+ * 
+ * ⁠       1
+ * ⁠      / \
+ * ⁠     2   3
+ * ⁠    /   / \
+ * ⁠   4   5   6
+ * ⁠      /
+ * ⁠     7
+ * 
+ * 输出:
+ * 7
+ * 
+ * 
+ * 
+ * 
+ * 注意: 您可以假设树（即给定的根节点）不为 NULL。
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int findBottomLeftValue(TreeNode root) {
+        Deque<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        TreeNode node = null;
+        while (!queue.isEmpty()) {
+            node = queue.poll();
+            if (node.right != null) {
+                queue.add(node.right);
+            }
+            if (node.left != null) {
+                queue.add(node.left);
+            }
+        }
+
+        return node.val;
+    }
+}
+// @lc code=end
+
