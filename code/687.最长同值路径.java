665d8e2d6eb4d65fc3434cd2850e19703fe4aa96
Jiang <Jiangqinhua1993@gmail.com>
2021-01-02 22:38:40 +0800

    Longest Univalue Path:DFS

---
+++ code/687.最长同值路径.java
@@ -0,0 +1,93 @@
+/*
+ * @lc app=leetcode.cn id=687 lang=java
+ *
+ * [687] 最长同值路径
+ *
+ * https://leetcode-cn.com/problems/longest-univalue-path/description/
+ *
+ * algorithms
+ * Medium (42.17%)
+ * Likes:    407
+ * Dislikes: 0
+ * Total Accepted:    27.1K
+ * Total Submissions: 63.9K
+ * Testcase Example:  '[5,4,5,1,1,5]'
+ *
+ * 给定一个二叉树，找到最长的路径，这个路径中的每个节点具有相同值。 这条路径可以经过也可以不经过根节点。
+ * 
+ * 注意：两个节点之间的路径长度由它们之间的边数表示。
+ * 
+ * 示例 1:
+ * 
+ * 输入:
+ * 
+ * 
+ * ⁠             5
+ * ⁠            / \
+ * ⁠           4   5
+ * ⁠          / \   \
+ * ⁠         1   1   5
+ * 
+ * 
+ * 输出:
+ * 
+ * 
+ * 2
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入:
+ * 
+ * 
+ * ⁠             1
+ * ⁠            / \
+ * ⁠           4   5
+ * ⁠          / \   \
+ * ⁠         4   4   5
+ * 
+ * 
+ * 输出:
+ * 
+ * 
+ * 2
+ * 
+ * 
+ * 注意: 给定的二叉树不超过10000个结点。 树的高度不超过1000。
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int ans = 0;
+    private int dfs(TreeNode root) {
+        if (root == null) return 0;
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+        left = root.left != null && root.val == root.left.val ? left + 1 : 0;
+        right = root.right != null && root.val == root.right.val ? right + 1 : 0;
+        ans = Math.max(ans, left + right);
+        return Math.max(left, right);
+    }
+    public int longestUnivaluePath(TreeNode root) {
+        dfs(root);
+        return ans;
+    }
+}
+// @lc code=end
+
