595eaf15c4d77bdf8f4ffbdef8af00c3d6998440
Jiang <Jiangqinhua1993@gmail.com>
2021-01-11 21:16:40 +0800

    Longest Substring Without Repeating Characters: Sliding Window

---
+++ code/3.无重复字符的最长子串.java
@@ -0,0 +1,85 @@
+/*
+ * @lc app=leetcode.cn id=3 lang=java
+ *
+ * [3] 无重复字符的最长子串
+ *
+ * https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/description/
+ *
+ * algorithms
+ * Medium (36.13%)
+ * Likes:    4802
+ * Dislikes: 0
+ * Total Accepted:    787.9K
+ * Total Submissions: 2.2M
+ * Testcase Example:  '"abcabcbb"'
+ *
+ * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+ * 
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 
+ * 输入: s = "abcabcbb"
+ * 输出: 3 
+ * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 
+ * 输入: s = "bbbbb"
+ * 输出: 1
+ * 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+ * 
+ * 
+ * 示例 3:
+ * 
+ * 
+ * 输入: s = "pwwkew"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+ * 请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+ * 
+ * 
+ * 示例 4:
+ * 
+ * 
+ * 输入: s = ""
+ * 输出: 0
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 0 
+ * s 由英文字母、数字、符号和空格组成
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int ans = 0;
+        int[] window = new int[128];
+        int l = 0, r = 0;
+        while (r < s.length()) {
+            char c = s.charAt(r);
+            window[c]++;
+            r++;
+            while (window[c] > 1) {
+                char d = s.charAt(l);
+                l++;
+                window[d]--;
+            }
+            ans = Math.max(ans, r - l);
+        }
+        ans = Math.max(r - l, ans);
+        return ans;
+    }
+}
+// @lc code=end
+
