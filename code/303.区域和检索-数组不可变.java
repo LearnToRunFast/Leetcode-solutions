22612b8a342c626dd15b6bea95cacc3aec8d7629
Jiang <Jiangqinhua1993@gmail.com>
2021-02-03 20:37:57 +0800

    303. Range Sum Query - Immutable: Prefix sum

--- code/303.区域和检索-数组不可变.java
+++ code/303.区域和检索-数组不可变.java
@@ -34,13 +34,16 @@
 
 // @lc code=start
 class NumArray {
-
+    private int[] dp;
     public NumArray(int[] nums) {
-        
+        dp = new int[nums.length + 1];
+        for (int i = 1; i <= nums.length; i++) {
+            dp[i] = dp[i - 1] + nums[i - 1];
+        }
     }
     
     public int sumRange(int i, int j) {
-        
+        return dp[j + 1] - dp[i];
     }
 }
 
