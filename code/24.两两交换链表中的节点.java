b40219989ca84dd6884a4f2233c8f74f440983d5
Jiang <Jiangqinhua1993@gmail.com>
2021-02-18 20:17:02 +0800

    24. Swap Nodes in Pairs: LinkedList

---
+++ code/24.两两交换链表中的节点.java
@@ -0,0 +1,89 @@
+/*
+ * @lc app=leetcode.cn id=24 lang=java
+ *
+ * [24] 两两交换链表中的节点
+ *
+ * https://leetcode-cn.com/problems/swap-nodes-in-pairs/description/
+ *
+ * algorithms
+ * Medium (68.86%)
+ * Likes:    817
+ * Dislikes: 0
+ * Total Accepted:    220.8K
+ * Total Submissions: 319.5K
+ * Testcase Example:  '[1,2,3,4]'
+ *
+ * 给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+ * 
+ * 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：head = [1,2,3,4]
+ * 输出：[2,1,4,3]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：head = []
+ * 输出：[]
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：head = [1]
+ * 输出：[1]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 链表中节点的数目在范围 [0, 100] 内
+ * 0 
+ * 
+ * 
+ * 
+ * 
+ * 进阶：你能在不修改链表节点值的情况下解决这个问题吗?（也就是说，仅修改节点本身。）
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    private void reverseTwoNode(ListNode curr) {
+        ListNode head = curr.next;
+        ListNode next = curr.next.next;
+        head.next = next.next;
+        next.next = head;
+        curr.next = next;
+    }
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+        ListNode curr = dummy;
+        while (curr.next != null && curr.next.next != null) {
+            reverseTwoNode(curr);
+            curr = curr.next.next;
+        }
+        return  dummy.next;
+    }
+}
+// @lc code=end
+
