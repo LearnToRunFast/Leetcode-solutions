84298500c58e1748e0361abaeb661615697c2893
Jiang <36619735+LearnToRunFast@users.noreply.github.com>
2021-03-24 18:21:08 +0800

    147. Insertion Sort List

---
+++ code/147.对链表进行插入排序.go
@@ -0,0 +1,84 @@
+/*
+ * @lc app=leetcode.cn id=147 lang=golang
+ *
+ * [147] 对链表进行插入排序
+ *
+ * https://leetcode-cn.com/problems/insertion-sort-list/description/
+ *
+ * algorithms
+ * Medium (67.26%)
+ * Likes:    368
+ * Dislikes: 0
+ * Total Accepted:    80.3K
+ * Total Submissions: 119.4K
+ * Testcase Example:  '[4,2,1,3]'
+ *
+ * 对链表进行插入排序。
+ *
+ *
+ * 插入排序的动画演示如上。从第一个元素开始，该链表可以被认为已经部分排序（用黑色表示）。
+ * 每次迭代时，从输入数据中移除一个元素（用红色表示），并原地将其插入到已排好序的链表中。
+ *
+ *
+ *
+ * 插入排序算法：
+ *
+ *
+ * 插入排序是迭代的，每次只移动一个元素，直到所有元素可以形成一个有序的输出列表。
+ * 每次迭代中，插入排序只从输入数据中移除一个待排序的元素，找到它在序列中适当的位置，并将其插入。
+ * 重复直到所有输入数据插入完为止。
+ *
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入: 4->2->1->3
+ * 输出: 1->2->3->4
+ *
+ *
+ * 示例 2：
+ *
+ * 输入: -1->5->3->4->0
+ * 输出: -1->0->3->4->5
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func insertionSortList(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	dummy := &ListNode{Next: head}
+	prev, curr := head, head.Next
+	for curr != nil {
+		if curr.Val >= prev.Val {
+			prev = prev.Next
+			curr = curr.Next
+			continue
+		}
+
+		pos := dummy
+		for pos.Next.Val <= curr.Val {
+			pos = pos.Next
+		}
+
+		prev.Next = curr.Next
+		curr.Next = pos.Next
+		pos.Next = curr
+
+		curr = prev.Next
+	}
+	return dummy.Next
+}
+
+// @lc code=end
+
