eccb4a7442f47da7454140bc8608b5461164e4fe
Jiang <36619735+LearnToRunFast@users.noreply.github.com>
2021-04-23 21:00:34 +0800

    260. Single Number III

---
+++ code/233.数字-1-的个数.go
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode.cn id=233 lang=golang
+ *
+ * [233] 数字 1 的个数
+ *
+ * https://leetcode-cn.com/problems/number-of-digit-one/description/
+ *
+ * algorithms
+ * Hard (39.44%)
+ * Likes:    211
+ * Dislikes: 0
+ * Total Accepted:    14.8K
+ * Total Submissions: 37.3K
+ * Testcase Example:  '13'
+ *
+ * 给定一个整数 n，计算所有小于等于 n 的非负整数中数字 1 出现的个数。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：n = 13
+ * 输出：6
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：n = 0
+ * 输出：0
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 0 
+ * 
+ * 
+ */
+
+// @lc code=start
+func countDigitOne(n int) int {
+
+}
+// @lc code=end
+
