061e5ba084f0fcdef12e15fe25f5424fef755f5a
Jiang <36619735+LearnToRunFast@users.noreply.github.com>
2021-05-14 12:42:26 +0800

    335. Self Crossing

---
+++ code/335.路径交叉.go
@@ -0,0 +1,86 @@
+/*
+ * @lc app=leetcode.cn id=335 lang=golang
+ *
+ * [335] 路径交叉
+ *
+ * https://leetcode-cn.com/problems/self-crossing/description/
+ *
+ * algorithms
+ * Hard (36.44%)
+ * Likes:    43
+ * Dislikes: 0
+ * Total Accepted:    2.4K
+ * Total Submissions: 6.7K
+ * Testcase Example:  '[2,1,1,2]'
+ *
+ * 给定一个含有 n 个正数的数组 x。从点 (0,0) 开始，先向北移动 x[0] 米，然后向西移动 x[1] 米，向南移动 x[2] 米，向东移动
+ * x[3] 米，持续移动。也就是说，每次移动后你的方位会发生逆时针变化。
+ *
+ * 编写一个 O(1) 空间复杂度的一趟扫描算法，判断你所经过的路径是否相交。
+ *
+ *
+ *
+ * 示例 1:
+ *
+ * ┌───┐
+ * │   │
+ * └───┼──>
+ * │
+ *
+ * 输入: [2,1,1,2]
+ * 输出: true
+ *
+ *
+ * 示例 2:
+ *
+ * ┌──────┐
+ * │      │
+ * │
+ * │
+ * └────────────>
+ *
+ * 输入: [1,2,3,4]
+ * 输出: false
+ *
+ *
+ * 示例 3:
+ *
+ * ┌───┐
+ * │   │
+ * └───┼>
+ *
+ * 输入: [1,1,1,1]
+ * 输出: true
+ *
+ *
+ */
+
+// @lc code=start
+func isSelfCrossing(dist []int) bool {
+	n := len(dist)
+	if n < 4 {
+		return false
+	}
+	i := 2
+	// out loop
+	for i < n && dist[i] > dist[i-2] {
+		i++
+	}
+	if i == n {
+		return false
+	}
+
+	if i == 3 && dist[i] == dist[i-2] ||
+		i >= 4 && dist[i] >= dist[i-2]-dist[i-4] {
+		dist[i-1] -= dist[i-3]
+	}
+	i++
+	// in loop
+	for i < n && dist[i] < dist[i-2] {
+		i++
+	}
+	return i < n
+}
+
+// @lc code=end
+
