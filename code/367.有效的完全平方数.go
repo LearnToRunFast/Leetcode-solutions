567b5f72fe3fd3b2c216e38e44ec81e498215d1b
Jiang <36619735+LearnToRunFast@users.noreply.github.com>
2021-05-21 18:18:47 +0800

    367. Valid Perfect Square

---
+++ code/367.有效的完全平方数.go
@@ -0,0 +1,59 @@
+/*
+ * @lc app=leetcode.cn id=367 lang=golang
+ *
+ * [367] 有效的完全平方数
+ *
+ * https://leetcode-cn.com/problems/valid-perfect-square/description/
+ *
+ * algorithms
+ * Easy (43.73%)
+ * Likes:    214
+ * Dislikes: 0
+ * Total Accepted:    63.6K
+ * Total Submissions: 145.4K
+ * Testcase Example:  '16'
+ *
+ * 给定一个 正整数 num ，编写一个函数，如果 num 是一个完全平方数，则返回 true ，否则返回 false 。
+ *
+ * 进阶：不要 使用任何内置的库函数，如  sqrt 。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：num = 16
+ * 输出：true
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：num = 14
+ * 输出：false
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1
+ *
+ *
+ */
+
+// @lc code=start
+func isPerfectSquare(num int) bool {
+	if num < 2 {
+		return true
+	}
+	x := num / 2
+	for x*x > num {
+		x = (x + num/x) / 2
+	}
+	return x*x == num
+}
+
+// @lc code=end
+
