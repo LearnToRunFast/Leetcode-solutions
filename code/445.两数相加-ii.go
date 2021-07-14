287b560bb2927fd79ad911ca1e2595e359f9cc07
Jiang <36619735+LearnToRunFast@users.noreply.github.com>
2021-07-14 17:34:10 +0800

    445. Add Two Numbers II

---
+++ code/445.两数相加-ii.go
@@ -0,0 +1,85 @@
+/*
+ * @lc app=leetcode.cn id=445 lang=golang
+ *
+ * [445] 两数相加 II
+ *
+ * https://leetcode-cn.com/problems/add-two-numbers-ii/description/
+ *
+ * algorithms
+ * Medium (58.53%)
+ * Likes:    404
+ * Dislikes: 0
+ * Total Accepted:    75K
+ * Total Submissions: 128.1K
+ * Testcase Example:  '[7,2,4,3]\n[5,6,4]'
+ *
+ * 给你两个 非空 链表来代表两个非负整数。数字最高位位于链表开始位置。它们的每个节点只存储一位数字。将这两数相加会返回一个新的链表。
+ *
+ * 你可以假设除了数字 0 之外，这两个数字都不会以零开头。
+ *
+ *
+ *
+ * 进阶：
+ *
+ * 如果输入链表不能修改该如何处理？换句话说，你不能对列表中的节点进行翻转。
+ *
+ *
+ *
+ * 示例：
+ *
+ * 输入：(7 -> 2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * 输出：7 -> 8 -> 0 -> 7
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	stack1 := []int{}
+	stack2 := []int{}
+	for l1 != nil {
+		stack1 = append(stack1, l1.Val)
+		l1 = l1.Next
+	}
+	for l2 != nil {
+		stack2 = append(stack2, l2.Val)
+		l2 = l2.Next
+	}
+	var head *ListNode
+
+	carry := 0
+	for len(stack1) > 0 || len(stack2) > 0 {
+		sum := carry
+		if len(stack1) > 0 {
+			sum += stack1[len(stack1)-1]
+			stack1 = stack1[:len(stack1)-1]
+		}
+		if len(stack2) > 0 {
+			sum += stack2[len(stack2)-1]
+			stack2 = stack2[:len(stack2)-1]
+		}
+		if sum >= 10 {
+			carry = 1
+			sum -= 10
+		} else {
+			carry = 0
+		}
+		newNode := &ListNode{Val: sum, Next: head}
+		head = newNode
+	}
+	if carry == 1 {
+		newNode := &ListNode{Val: carry, Next: head}
+		head = newNode
+	}
+	return head
+}
+
+// @lc code=end
+
