6f51a0ae6e7f6d5380b6d6b924e185bb5d3c6a8c
Jiang <36619735+LearnToRunFast@users.noreply.github.com>
2021-08-17 21:05:47 +0800

    Create 541.反转字符串-ii.go

---
+++ code/541.反转字符串-ii.go
@@ -0,0 +1,75 @@
+/*
+ * @lc app=leetcode.cn id=541 lang=golang
+ *
+ * [541] 反转字符串 II
+ *
+ * https://leetcode-cn.com/problems/reverse-string-ii/description/
+ *
+ * algorithms
+ * Easy (58.03%)
+ * Likes:    139
+ * Dislikes: 0
+ * Total Accepted:    43.5K
+ * Total Submissions: 74.8K
+ * Testcase Example:  '"abcdefg"\n2'
+ *
+ * 给定一个字符串 s 和一个整数 k，从字符串开头算起，每 2k 个字符反转前 k 个字符。
+ *
+ *
+ * 如果剩余字符少于 k 个，则将剩余字符全部反转。
+ * 如果剩余字符小于 2k 但大于或等于 k 个，则反转前 k 个字符，其余字符保持原样。
+ *
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：s = "abcdefg", k = 2
+ * 输出："bacdfeg"
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：s = "abcd", k = 2
+ * 输出："bacd"
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1 <= s.length <= 10^4
+ * s 仅由小写英文组成
+ * 1 <= k <= 10^4
+ *
+ *
+ */
+
+// @lc code=start
+func reverse(s []byte) {
+	i, j := 0, len(s)-1
+	for i < j {
+		s[i], s[j] = s[j], s[i]
+		i++
+		j--
+	}
+
+}
+func reverseStr(s string, k int) string {
+	b := []byte(s)
+	n := len(s)
+	for i := 0; i < n; i += 2 * k {
+		if i+k < n {
+			reverse(b[i : i+k])
+		} else {
+			reverse(b[i:])
+		}
+	}
+	return string(b)
+}
+
+// @lc code=end
+
