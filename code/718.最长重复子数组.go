7a9b74d6710dcd243aff08c280881466264c1d4c
Jiang <36619735+LearnToRunFast@users.noreply.github.com>
2021-09-13 16:21:33 +0800

    Update 718.最长重复子数组.go

--- code/718.最长重复子数组.go
+++ code/718.最长重复子数组.go
@@ -39,30 +39,110 @@
  */
 
 // @lc code=start
-func max(a, b int) int {
-	if a > b {
+// rabin karp
+const (
+	base, mod = 101, 1 << 31
+)
+
+// 使用快速幂计算 x^n % mod 的值
+func qPow(x, n int) int {
+	res := 1
+	for n > 0 {
+		if n&1 == 1 {
+			res = res * x % mod
+		}
+		x = x * x % mod
+		n >>= 1
+	}
+	return res
+}
+func min(a, b int) int {
+	if a < b {
 		return a
 	}
 	return b
 }
-func findLength(nums1 []int, nums2 []int) int {
-	// define dp[i][j] as the longest common subarray of nums1[:i] and nums2[:j]
-	m, n := len(nums1), len(nums2)
-	dp := make([][]int, m+1)
-	for i := range dp {
-		dp[i] = make([]int, n+1)
+func getHash(nums []int, j int) int {
+	hash := 0
+	for i := 0; i < j; i++ {
+		hash = (hash*base + nums[i]) % mod
 	}
-	res := 0
-	for i := 1; i <= m; i++ {
-		for j := 1; j <= n; j++ {
-			if nums1[i-1] == nums2[j-1] {
-				dp[i][j] = dp[i-1][j-1] + 1
-				res = max(res, dp[i][j])
-			}
+	return hash
+}
+func equalArr(nums1 []int, nums2 []int) bool {
+	if len(nums1) != len(nums2) {
+		return false
+	}
+	for i := 0; i < len(nums1); i++ {
+		if nums1[i] != nums2[i] {
+			return false
 		}
 	}
-	return res
+	return true
 }
+func check(length int, nums1, nums2 []int) bool {
+	hashA := getHash(nums1, length)
+	mult := qPow(base, length-1)
+	mapA := map[int][]int{hashA: []int{0, length}}
+	for i := length; i < len(nums1); i++ {
+		toRemove := nums1[i-length] * mult % mod
+		// in case if toRemove more than hashA
+		hashA = ((hashA-toRemove+mod)%mod*base + nums1[i]) % mod
+		mapA[hashA] = []int{i - length + 1, i + 1}
+	}
+
+	hashB := getHash(nums2, length)
+	if v, ok := mapA[hashB]; ok {
+		return equalArr(nums1[v[0]:v[1]], nums2[0:length])
+	}
+	for i := length; i < len(nums2); i++ {
+		toRemove := nums2[i-length] * mult % mod
+		hashB = ((hashB-toRemove+mod)%mod*base + nums2[i]) % mod
+		if v, ok := mapA[hashB]; ok {
+			return equalArr(nums1[v[0]:v[1]], nums2[i-length+1:i+1])
+		}
+	}
+	return false
+}
+func findLength(nums1 []int, nums2 []int) int {
+	// start at the length of half, if there is a match,
+	// increase the length by 1/4, if not, decrease the length by 1/4
+	left, right := 1, min(len(nums1), len(nums2))+1
+	for left < right {
+		mid := (left + right) >> 1
+		if check(mid, nums1, nums2) {
+			left = mid + 1
+		} else {
+			right = mid
+		}
+	}
+	return left - 1
+}
+
+// func max(a, b int) int {
+// 	if a > b {
+// 		return a
+// 	}
+// 	return b
+// }
+// func findLength(nums1 []int, nums2 []int) int {
+// 	// define dp[i][j] as the longest common subarray of nums1[:i] and nums2[:j]
+// 	m, n := len(nums1), len(nums2)
+// 	dp := make([][]int, m+1)
+// 	for i := range dp {
+// 		dp[i] = make([]int, n+1)
+// 	}
+// 	res := 0
+// 	for i := 1; i <= m; i++ {
+// 		for j := 1; j <= n; j++ {
+// 			if nums1[i-1] == nums2[j-1] {
+// 				dp[i][j] = dp[i-1][j-1] + 1
+// 				res = max(res, dp[i][j])
+// 			}
+// 		}
+// 	}
+// 	return res
+// }
 
 // @lc code=end
 
