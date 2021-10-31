588778c2f9278c138391e8eda6842db6f64e8d67
Jiang <36619735+LearnToRunFast@users.noreply.github.com>
2021-10-31 23:20:33 +0800

    kmp

---
+++ code/28.实现-str-str.go
@@ -0,0 +1,107 @@
+/*
+ * @lc app=leetcode.cn id=28 lang=golang
+ *
+ * [28] 实现 strStr()
+ *
+ * https://leetcode-cn.com/problems/implement-strstr/description/
+ *
+ * algorithms
+ * Easy (40.59%)
+ * Likes:    1090
+ * Dislikes: 0
+ * Total Accepted:    488.8K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '"hello"\n"ll"'
+ *
+ * 实现 strStr() 函数。
+ *
+ * 给你两个字符串 haystack 和 needle ，请你在 haystack 字符串中找出 needle 字符串出现的第一个位置（下标从 0
+ * 开始）。如果不存在，则返回  -1 。
+ *
+ *
+ *
+ * 说明：
+ *
+ * 当 needle 是空字符串时，我们应当返回什么值呢？这是一个在面试中很好的问题。
+ *
+ * 对于本题而言，当 needle 是空字符串时我们应当返回 0 。这与 C 语言的 strstr() 以及 Java 的 indexOf()
+ * 定义相符。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：haystack = "hello", needle = "ll"
+ * 输出：2
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：haystack = "aaaaa", needle = "bba"
+ * 输出：-1
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：haystack = "", needle = ""
+ * 输出：0
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 0
+ * haystack 和 needle 仅由小写英文字符组成
+ *
+ *
+ */
+
+// @lc code=start
+func strStr(haystack string, needle string) int {
+	n, m := len(haystack), len(needle)
+	if m == 0 {
+		return 0
+	}
+	if n < m {
+		return -1
+	}
+
+	// create prefix table with m + 1 size
+	// index 0 as placeholder
+	prefix := make([]int, m)
+	j := 0
+	for i := 1; i < m; i++ {
+		for j > 0 && needle[i] != needle[j] {
+			j = prefix[j-1]
+		}
+		if needle[i] == needle[j] {
+			j++
+		}
+		prefix[i] = j
+	}
+
+	// search
+	j = 0
+	for i := range haystack {
+		for j > 0 && haystack[i] != needle[j] {
+			j = prefix[j-1]
+		}
+
+		if haystack[i] == needle[j] {
+			j++
+			if j == m {
+				return i - m + 1
+			}
+		}
+	}
+
+	return -1
+}
+
+// @lc code=end
+
